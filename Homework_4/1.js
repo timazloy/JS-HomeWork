bfcf3f3cca4a6198413b047ee6d553e451af870e
Tima <timazloy04444@gmail.com>
2021-07-04 17:16:22 +0300

    add the yusk number 1

--- Homework_4/1.js
+++ Homework_4/1.js
@@ -2,15 +2,21 @@
 
 function getObject(num) {
     if (!Number.isInteger(num) || num < 0 || num > 999) {
-        let units = (num % 10);
-        let tens = 55
-        let hundreds = Math.floor(num / 100);
-
-    } else {
         alert("Число должно быть целым, в диапазоне от 0 до 999");
 
+    } else {
+        function Number(num) {
+            this.units = (num % 10);
+            this.tens = Math.floor(num % 100 / 10);
+            this.hundreds = Math.floor(num / 100);
+        }
+        const a = new Number(num);
+        console.log(a);
     }
-
 }
 
-// еще не решил
+let a = +prompt("Введите цело число от 0 до 999: ")
+getObject(a);
+
+
+//
